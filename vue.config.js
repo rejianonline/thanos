d264dc309f49bee462cfac6ec6a8d83b5cb2a21e
zhengzhigang <813726742@qq.com>
2020-01-11 22:57:07 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/manage-static/',
+  publicPath: '/manage/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
