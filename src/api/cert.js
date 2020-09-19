e41caf0dd45cda0c897319eec913c4473a5bde07
郑智刚 <zhengzhigang@igengmei.com>
2020-09-19 22:38:19 +0800

    fix bug

--- src/api/cert.js
+++ src/api/cert.js
@@ -158,7 +158,7 @@ export function certPoseImg(data = {}) {
 // 实名认证接口
 export function certCardImg(data = {}) {
   return request({
-    url: `/adminApi/admin/identify/verifyId?accId=${data.accId}&cardStatus=${data.cardStatus}&bodyStatus=${data.bodyStatus}`,
+    url: `/adminApi/admin/identify/verifyId?accId=${data.accId}&cardStatus=${data.cardStatus}`,
     method: 'post'
   })
 }
